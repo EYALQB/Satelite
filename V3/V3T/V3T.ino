3a4034f16ac7656915d73f76063b8e60eb2e89a3
JoanPoAl <126171830+JoanPoAl@users.noreply.github.com>
2024-11-20 08:16:50 +0100

    Update V3T.ino

--- V3/V3T/V3T.ino
+++ V3/V3T/V3T.ino
@@ -6,7 +6,7 @@ SoftwareSerial mySerial(11, 10); // RX, TX (azul, naranja)
 Servo servoMotor;
 
 //Pines
-#define DHTPIN 2
+#define DHTPIN 13
 int Trig1 = 3;
 int Echo1 = 4;
 int Trig2 = 5;
@@ -14,14 +14,14 @@ int Echo2 = 6;
 
 //Variables Ambiente
 #define DHTTYPE DHT11
-unsigned long tiempoAmb; //Temporizador de ambiente
+unsigned long tiempoAmb=0; //Temporizador de ambiente
 
 //Variables ditsancia
 int inc=0;
-int cambio=0;//Nos servirá para rotar el servo en los dos sentidos
+int cambio=1;//Nos servirá para rotar el servo en los dos sentidos
 const float VelSon = 34000.0;// Constante velocidad sonido en cm/s
 float d1, d2;
-unsigned long tiempoDist; //Temporizador del servo
+unsigned long tiempoDist=0; //Temporizador del servo
 
 
 
@@ -44,49 +44,48 @@ void setup() {
 void ambiente(){
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  Serial.print("Amb ");
-  Serial.print(t);
-  Serial.print(" ");
-  Serial.print(h);
-  Serial.print(" ");
-  Serial.print(h);
-  Serial.print(" ");
-  Serial.println(h);
+  mySerial.print("Amb ");
+  mySerial.print(t);
+  mySerial.print(" ");
+  mySerial.println(h);
 }
+void radar(int *inc){
+  //En qué sentido gira el servo
+  if (*inc==180){
+    cambio = -1;
+  }
+  if (*inc==0){
+    cambio = 1;
+  }
   
+  servoMotor.write(*inc);
+  digitalWrite(Trig1, LOW);
+  digitalWrite(Trig1, HIGH);
+  unsigned long tiempo1 = pulseIn(Echo1, HIGH);
+  d1 = tiempo1 * 0.000001 * VelSon / 2; //Fórmula para calcular la distancia en cm
+  
+  digitalWrite(Trig2, LOW);
+  digitalWrite(Trig2, HIGH);
+  unsigned long tiempo2 = pulseIn(Echo2, HIGH);
+  d2 = tiempo2 * 0.000001 * VelSon / 2; //Fórmula para calcular la distancia en cm
+  mySerial.print("Dis ");
+  mySerial.print(d1);
+  mySerial.print(" ");
+  mySerial.print(*inc);
+  mySerial.print(" ");
+  mySerial.println(d2);
+  *inc=*inc+10*cambio; //Cambiamos la inclinación en 10 grados en el sentido que nos indique el cambio
+}
+
 void loop() {
   //Ambiente
-  if (millis()>=tiempoAmb){
+  if (millis()-tiempoAmb>=1000){
     ambiente();
-    tiempoAmb=millis()+1000;
+    tiempoAmb=millis();
   }
   //Servo
-  if (millis()>=tiempoDist){
-    //En qué sentido gira el servo
-    if (inc==180){
-      cambio = -1;
-    }
-    if (inc==0){
-      cambio = 1;
-    }
-    
-    servoMotor.write(inc);
-    digitalWrite(Trig1, LOW);
-    digitalWrite(Trig1, HIGH);
-    unsigned long tiempo1 = pulseIn(Echo1, HIGH);
-    d1 = tiempo1 * 0.000001 * VelSon / 2; //Fórmula para calcular la distancia en cm
-    
-    //digitalWrite(Trig2, LOW);
-    //digitalWrite(Trig2, HIGH);
-    //unsigned long tiempo2 = pulseIn(Echo2, HIGH);
-    //d2 = tiempo2 * 0.000001 * VelSon / 2; //Fórmula para calcular la distancia en cm
-    Serial.print("Dis ");
-    Serial.print(d1);
-    Serial.print(" ");
-    Serial.print(inc);
-    Serial.print(" ");
-    Serial.println(d1);
-    inc=inc+10*cambio; //Cambiamos la inclinación en 10 grados en el sentido que nos indique el cambio
-    tiempoDist=millis()+100;
+  if (millis()-tiempoDist>=500){
+    radar(&inc);
+    tiempoDist=millis();
   }
 }
